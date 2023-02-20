b69eb461e99307b94b2a9215fc1d6d3ca47b69ee
MdAbdullahAlMahmud <mmunna864@gmail.com>
2023-02-20 22:52:58 +0600

    Project setup completed

---
+++ app/src/main/java/com/example/onlineshoppingbs23/ui/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.example.onlineshoppingbs23.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.onlineshoppingbs23.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
---
+++ app/src/main/java/com/example/onlineshoppingbs23/ui/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.onlineshoppingbs23.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.onlineshoppingbs23.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent= new Intent(SplashActivity.this,RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
